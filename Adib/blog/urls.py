9502e161062416574c36ef33269d89aad78528c1
Bauer_san <bauereudes225@gmail.com>
2024-03-22 10:49:12 +0000

    fix errors on they urls

--- Adib/blog/urls.py
+++ Adib/blog/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('blog/', ListHouseInfoView.as_view()),
-    path('<int:pk>/', DetailHouseInfoView.as_view()),
+    path('blog/<int:pk>/', DetailHouseInfoView.as_view()),
     path('contact/', CreateContactView.as_view()),
 
 ]
